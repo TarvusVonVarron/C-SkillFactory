18947fff922904f846dd4b477ba1f26bac6f8e6b
tarvusvarron97 <tarvusvarron97@gmail.com>
2024-05-24 16:19:04 +0300

    ещё немного ООП

--- СSharpLang/Module6/Task5/Task5.cs
+++ СSharpLang/Module6/Task5/Task5.cs
@@ -8,6 +8,49 @@ namespace Module6
 {
     public static class Task5
     {
+        public class Square
+        {
+            int a;
+            public int Area()
+            { 
+                return a*a; 
+            }
+
+            public int Perimeter()
+            {
+                return a*4;
+            }
+        }
+
+        public class Triangle
+        {
+            int a;
+            int b;
+            int c;
+            public double Area()
+            {
+                double p = (a+b+c)/2;
+                return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            }
+            public int Perimeter()
+            {
+                return a+b+c;
+            }
+        }
+
+        public class Round
+        {
+            int r;
+
+            public double Area()
+            {
+                return Math.PI * r * r;
+            }
+            public double Perimeter()
+            {
+                return Math.PI * r * 2;
+            }
+        }
         public static void Run()
         {
 
---
+++ СSharpLang/Module6/Task6/Task6.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module6
+{
+    internal class Task6
+    {
+
+        public class TrafficLight
+        {
+            private void ChangeColor(string color)
+            {
+
+            }
+            public string GetColor()
+            {
+                return string.Empty;  
+            }
+        }
+
+        class User
+        {
+            private int age;
+            private string login;
+            private string mail;
+
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+
+                set
+                {
+                    if (value < 18)
+                    {
+                        Console.WriteLine("Возраст должен быть не меньше 18");
+                    }
+                    else
+                    {
+                        age = value;
+                    }
+                }
+            }
+
+            public string Login
+            {
+                get { return login; }
+                set
+                {
+                    if (value.Length<3)
+                    {
+                        Console.WriteLine("At least 3 characters");
+                    }
+                    else
+                    {
+                        login = value;
+                    }
+                }
+            }
+
+            public string Mail
+            {
+                get { return mail; }
+                set
+                {
+                    if (!value.Contains('@'))
+                    {
+                        Console.WriteLine("The mail must contains @ character");
+                    }
+                    else
+                    {
+                        mail = value;
+                    }
+                }
+            }
+        }
+
+        public void Run() 
+        { 
+        
+        }
+    }
+}
