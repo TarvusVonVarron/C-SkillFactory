fb0bf4e815bfd279bd8202dec4c980fe4bb6d4e8
Denis <tarvusvarron97@gmail.com>
2024-05-29 17:52:38 +0300

    Создал проект для модуля 8, посвящённого работе с файлами

---
+++ СSharpLang/Module8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер задания:");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Task1.Run();
+                    break;
+                default:
+                    Console.WriteLine("Неправильный ввод.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
