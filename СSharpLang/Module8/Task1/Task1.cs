ef8ff3ae770db9dbeeb12ff9ff5d917d442fe65c
Denis <tarvusvarron97@gmail.com>
2024-06-01 16:10:00 +0300

    Начало изучения модуля по работе с файлами

--- СSharpLang/Module8/Task1/Task1.cs
+++ СSharpLang/Module8/Task1/Task1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,28 @@ namespace Module8
 {
     internal class Task1
     {
+        public class Folder
+        {
+            public List<string> Files { get; set; } = new List<string>();
+        }
+        class Drive
+        {
+            string Name { get; }
+            long TotalSpace { get; }
+            long FreeSpace { get; }
+
+            public Drive(string Name, int Space, double FreeSpace)
+            {
+                this.Name = Name;
+                this.Space = Space;
+                this.FreeSpace = FreeSpace;
+            }
+            Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+            public void CreateFolder(string name)
+            {
+                Folders.Add(name, new Folder());
+            }
+        }
         public static void Run()
         {
 
---
+++ СSharpLang/Module8/Task2/Task2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8.Task2
+{
+    internal class Task2
+    {
+
+        public static void Run()
+        {
+
+        }
+    }
+}
